c8fd43dc4a8bb9dc990a0b96516a61ee1069b423
RafaelHerBer <franciscor.herber@usal.es>
2025-03-13 19:45:41 +0100

    Update script.js

--- multiple-color-schemes/script.js
+++ multiple-color-schemes/script.js
@@ -48,3 +48,7 @@ function setWaspStyle(){
     let styleLink = document.getElementById("styleLink");
     styleLink.setAttribute("href","wasp-styles.css");
 }
+function setBlueStyle(){
+    let styleLink = document.getElementById("styleLink");
+    styleLink.setAttribute("href","blue-styles.css");
+}
